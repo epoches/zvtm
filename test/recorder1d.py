befefd8c9bd8bfaaf663aaba16513b789e59cee7
epoches <epoches@sohu.com>
2022-03-04 17:35:03 +0800

    1d更新

--- test/recorder1d.py
+++ test/recorder1d.py
@@ -2,5 +2,6 @@
 from zvtm.domain import  Stock1dHfqKdata,StockValuation,Stock1dKdata
 
 #Stock1dHfqKdata.record_data(provider='joinquant', sleeping_time=0, day_data=True)
-StockValuation.record_data(provider='joinquant', sleeping_time=0)
-#Stock1dKdata.record_data(provider='joinquant', sleeping_time=0)
+#StockValuation.record_data(provider='joinquant', sleeping_time=0)
+Stock1dKdata.record_data(provider='joinquant', sleeping_time=0,
+    start_timestamp = '2022-01-01', end_timestamp = '2022-03-02')#,code='000156',force_update=True,

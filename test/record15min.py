a54d2cbc6df4d60ab77aceee261c71da9b664894
epoches <epoches@sohu.com>
2022-03-10 17:33:56 +0800

    15分钟和5分钟

---
+++ test/record15min.py
@@ -0,0 +1,6 @@
+#保存数据到数据库,5分钟需要保存的
+from zvtm.domain import Stock15mHfqKdata
+
+
+Stock15mHfqKdata.record_data(provider='joinquant',code='002382', sleeping_time=0,
+    start_timestamp = '2021-03-09', end_timestamp = '2022-03-09')#,code='000156',force_update=True,

1bcb985bc1cb5c6b0a16b26098dca5527a858907
epoches <epoches@sohu.com>
2022-03-11 16:55:31 +0800

    5分钟

--- test/record5min.py
+++ test/record5min.py
@@ -3,4 +3,4 @@ from zvtm.domain import Stock5mHfqKdata
 
 
 Stock5mHfqKdata.record_data(provider='joinquant',code='002382', sleeping_time=0,
-    start_timestamp = '2022-03-09', end_timestamp = '2022-03-09')#,code='000156',force_update=True,
+   )#,code='000156',force_update=True,

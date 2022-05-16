9a76e29ff53791384a43ff053537f5ffa9851d93
epoches <epoches@sohu.com>
2022-05-16 18:18:50 +0800

    增加了龙虎榜

--- test/emrecorder.py
+++ test/emrecorder.py
@@ -15,13 +15,13 @@ from zvtm.informer.informer import EmailInformer
 #IncomeStatement.record_data(provider='eastmoney', sleeping_time=5)
 #CashFlowStatement.record_data(provider='eastmoney', sleeping_time=5)
 #DividendFinancing.record_data(provider='eastmoney', sleeping_time=5)
-#HolderTrading.record_data(provider='eastmoney', sleeping_time=5) #出错了 id有问题 stock_sz_000961_2022-01-12_中南城市建设投资有限公司
+HolderTrading.record_data(provider='eastmoney', sleeping_time=5) #出错了 id有问题 stock_sz_000961_2022-01-12_中南城市建设投资有限公司
 #ManagerTrading.record_data(provider='eastmoney', sleeping_time=5) #和上一个一样
-TopTenHolder.record_data(provider='eastmoney', sleeping_time=5)
-TopTenTradableHolder.record_data(provider='eastmoney', sleeping_time=5)
-StockInstitutionalInvestorHolder.record_data(provider='em', sleeping_time=5)
-StockTopTenFreeHolder.record_data(provider='em', sleeping_time=5)
-StockActorSummary.record_data(provider='em', sleeping_time=5)
-Index.record_data(provider='exchange', sleeping_time=5)
-IndexStock.record_data(provider='exchange', sleeping_time=5)
-Index1dKdata.record_data(provider='em', sleeping_time=5)
+# TopTenHolder.record_data(provider='eastmoney', sleeping_time=5)
+# TopTenTradableHolder.record_data(provider='eastmoney', sleeping_time=5)
+# StockInstitutionalInvestorHolder.record_data(provider='em', sleeping_time=5)
+# StockTopTenFreeHolder.record_data(provider='em', sleeping_time=5)
+# StockActorSummary.record_data(provider='em', sleeping_time=5)
+# Index.record_data(provider='exchange', sleeping_time=5)
+# IndexStock.record_data(provider='exchange', sleeping_time=5)
+# Index1dKdata.record_data(provider='em', sleeping_time=5)

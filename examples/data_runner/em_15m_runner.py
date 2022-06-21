c0988a35f261a3abea4ac0902ab8cdc850ac19cb
epoches <epoches@sohu.com>
2022-06-21 16:17:32 +0800

    em 15m

---
+++ examples/data_runner/em_15m_runner.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# emfinace更新容易出错
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from zvtm.domain import StockTradeDay
+
+logger = logging.getLogger(__name__)
+
+sched = BackgroundScheduler()
+
+# -*- coding: utf-8 -*-
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from examples.recorder_utils import run_data_recorder
+from zvtm import init_log
+from zvtm.domain import  Stock15mHfqKdata
+logger = logging.getLogger(__name__)
+
+sched = BackgroundScheduler()
+
+
+@sched.scheduled_job('cron',day_of_week='mon-fri', hour=15, minute=10)
+def record_stock_data(data_provider="em", entity_provider="em"):
+    # A股15m hfq
+    run_data_recorder(
+        domain=Stock15mHfqKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=1,
+    )
+# code = '000683',
+
+
+if __name__ == "__main__":
+    init_log("eastmoney_15m_runner.log")
+
+    record_stock_data()
+
+    sched.start()
+
+    sched._thread.join()
--- test/record15min.py
+++ test/record15min.py
@@ -1,6 +1,6 @@
 #保存数据到数据库,5分钟需要保存的
 from zvtm.domain import Stock15mHfqKdata
 
-
-Stock15mHfqKdata.record_data(provider='joinquant',code='000156', sleeping_time=0,
-    start_timestamp = '2015-02-11', end_timestamp = '2015-2-17')#,code='000156',force_update=True
+Stock15mHfqKdata.record_data(provider='em', sleeping_time=1)
+# Stock15mHfqKdata.record_data(provider='joinquant',code='000156', sleeping_time=0,
+#     start_timestamp = '2015-02-11', end_timestamp = '2015-2-17')#,code='000156',force_update=True

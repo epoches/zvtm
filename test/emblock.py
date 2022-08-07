0eb4161a90c62542a9398bdc362afbee9f411d2a
epoches <epoches@sohu.com>
2022-08-07 21:09:24 +0800

    em joinquant

--- test/emblock.py
+++ test/emblock.py
@@ -3,5 +3,5 @@ from zvtm.domain import Block,BlockStock,Block1dKdata,Block1wkKdata,Block1monKda
 #BlockStock.record_data(provider="eastmoney")
 # Block.record_data(provider="em")
 Block1dKdata.record_data(provider="em")
-Block1wkKdata.record_data(provider="em")
-Block1monKdata.record_data(provider="em")
+# Block1wkKdata.record_data(provider="em")
+# Block1monKdata.record_data(provider="em")
--- test/recorder1d.py
+++ test/recorder1d.py
@@ -2,9 +2,9 @@
 from zvtm.domain import  Stock1dHfqKdata,StockValuation,Stock1dKdata,Stock,StockSummary
 
 #Stock.record_data(provider='joinquant', sleeping_time=0, day_data=True)
-#Stock1dHfqKdata.record_data(provider='joinquant', sleeping_time=0, day_data=True)
+Stock1dHfqKdata.record_data(provider='joinquant', sleeping_time=0, day_data=True)
 #StockValuation.record_data(provider='joinquant', sleeping_time=0)
 # Stock1dKdata.record_data(provider='joinquant', sleeping_time=0,code='000156'
 #      )#,code='000156',force_update=True,start_timestamp = '2022-01-01', end_timestamp = '2022-03-02'
-StockSummary.record_data(provider='joinquant', sleeping_time=0
-     )
+# StockSummary.record_data(provider='joinquant', sleeping_time=0
+#      )

fc673ad5aa389266a416872607e1fb73bbf7dfb9
epoches <epoches@sohu.com>
2022-08-09 15:22:15 +0800

    每分钟定时任务

--- examples/data_runner/joinquant_1min_runner.py
+++ examples/data_runner/joinquant_1min_runner.py
@@ -1,3 +1,4 @@
+# 9:30开始 周一到周五 股市运行期间对指定数据进行更新
 # -*- coding: utf-8 -*-
 import logging
 
@@ -48,16 +49,17 @@ def record_stock_data(data_provider="joinquant", entity_provider="joinquant"):
     # A股后复权行情
     while True:
         now = datetime.datetime.now()
-        if ((int(now.strftime("%H")) >= 9 and int(now.strftime("%M")) >= 30) and (int(now.strftime("%H")) <= 11 and int(now.strftime("%M")) <= 30)) or (int(now.strftime("%H")) >= 13  and int(now.strftime("%H")) < 15):
+        if ((int(now.strftime("%H")) >= 9) and (int(now.strftime("%H")) <= 12)) or (int(now.strftime("%H")) >= 13  and int(now.strftime("%H")) < 15):
+            # Stock1mHfqKdata.record_data(provider='joinquant', sleeping_time=0, codes=codes)
             run_data_recorder(
                 domain=Stock1mHfqKdata,
                 data_provider=data_provider,
                 entity_provider=entity_provider,
                 day_data=False,
                 sleeping_time=0,
-                start_timestamp= datetime.datetime.now() + datetime.timedelta(seconds=-60),
-                codes = codes,
-            ) #start_timestamp='2022-04-01'
+                codes=codes,
+            )
+            #start_timestamp='2022-04-01' start_timestamp= datetime.datetime.now() + datetime.timedelta(seconds=-60),
             time.sleep(60)
         else:
             break
@@ -66,5 +68,17 @@ def record_stock_data(data_provider="joinquant", entity_provider="joinquant"):
 if __name__ == "__main__":
     init_log("joinquant_1min_runner.log")
     record_stock_data()
+
     sched.start()
+
     sched._thread.join()
+
+    # scheduler = BlockingScheduler()
+    # scheduler.add_job(record_data, 'cron', hour=14, minute=52)
+    #
+    # try:
+    #     scheduler.start()
+    # except (KeyboardInterrupt, SystemExit):
+    #     pass
+
+

5c59068e496013dd8be51c688915205f3bd1c299
epoches <epoches@sohu.com>
2022-12-12 14:32:28 +0800

    1m 5m

--- test/em_stock_1m.py
+++ test/em_stock_1m.py
@@ -1,2 +1,3 @@
-from zvtm.domain import Stock1mHfqKdata
-Stock1mHfqKdata.record_data(provider='em', sleeping_time=1)
+from zvtm.domain import Stock1mKdata,Stock1mHfqKdata
+Stock1mKdata.record_data(provider='em', sleeping_time=0)
+Stock1mHfqKdata.record_data(provider='em', sleeping_time=0)
---
+++ test/join_1m.py
@@ -0,0 +1,3 @@
+from zvtm.domain import Stock1mKdata,Stock1mHfqKdata
+Stock1mKdata.record_data(provider='joinquant', sleeping_time=0)
+Stock1mHfqKdata.record_data(provider='joinquant', sleeping_time=0)

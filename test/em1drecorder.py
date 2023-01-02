16ff25b7c81ee393e504ac1ba23c8cd2a77c3a72
epoches <epoches@sohu.com>
2023-01-02 19:49:02 +0800

    Merge branch 'main' of https://gitee.com/epoches/zvtm into main
    # Conflicts:
    #	test/recorder1d.py

--- test/em1drecorder.py
+++ test/em1drecorder.py
@@ -1,3 +1,3 @@
 from zvtm.domain import Stock1dHfqKdata
-Stock1dHfqKdata.record_data(provider='em', sleeping_time=0)
+Stock1dHfqKdata.record_data(provider='em', sleeping_time=0,code='000013',start_timestamp='2022-01-01')
 
---
+++ test/em_stock_15m.py
@@ -0,0 +1,3 @@
+from zvtm.domain import Stock15mKdata,Stock15mHfqKdata
+Stock15mKdata.record_data(provider='em', sleeping_time=0)
+Stock15mHfqKdata.record_data(provider='em', sleeping_time=0)

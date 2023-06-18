6d1ebfeeb502507611704eae5c3bf95b112c8e96
epoches <epoches@sohu.com>
2023-06-18 20:54:57 +0800

    修改了一些问题。

--- test/em_stock_5m.py
+++ test/em_stock_5m.py
@@ -1,3 +1,3 @@
 from zvtm.domain import Stock5mKdata,Stock5mHfqKdata
-Stock5mKdata.record_data(provider='em', sleeping_time=0)
-Stock5mHfqKdata.record_data(provider='em', sleeping_time=0)
+# Stock5mKdata.record_data(provider='em', sleeping_time=0,end_timestamp='2022-10-24',code='601012')
+Stock5mHfqKdata.record_data(provider='em', sleeping_time=0,code='601012')

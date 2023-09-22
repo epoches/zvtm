aa5fe65e4dde8a84e9603132f36ac66ba1cc62fe
epoches <epoches@sohu.com>
2023-09-22 19:46:40 +0800

    开始每日对5 1 15 60分钟数据进行更新

--- examples/data_runner/em_kdata_runner.py
+++ examples/data_runner/em_kdata_runner.py
@@ -2,7 +2,7 @@
 import logging
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from zvtm.domain import Block,BlockStock,Block1dKdata,Block1wkKdata,Block1monKdata
+from zvtm.domain import Block,BlockStock,Block1dKdata,Block1wkKdata,Block1monKdata,Stock1hHfqKdata,Block1hKdata,Stock15mHfqKdata,Block15mKdata,Stock5mHfqKdata,Block5mKdata,Stock1mHfqKdata,Block1mKdata
 
 
 logger = logging.getLogger(__name__)
@@ -53,6 +53,66 @@ def record_stock_data(data_provider="em", entity_provider="em"):
     # block
     run_data_recorder(
         domain=Block1dKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    # A股60m hfq
+    run_data_recorder(
+        domain=Stock1hHfqKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    run_data_recorder(
+        domain=Block1hKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    # A股15m hfq
+    run_data_recorder(
+        domain=Stock15mHfqKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    run_data_recorder(
+        domain=Block15mKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    # A股5m hfq
+    run_data_recorder(
+        domain=Stock5mHfqKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    run_data_recorder(
+        domain=Block5mKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    # A股5m hfq
+    run_data_recorder(
+        domain=Stock1mHfqKdata,
+        data_provider=data_provider,
+        entity_provider=entity_provider,
+        day_data=False,
+        sleeping_time=0,
+    )
+    run_data_recorder(
+        domain=Block1mKdata,
         data_provider=data_provider,
         entity_provider=entity_provider,
         day_data=False,
--- test/1mrecord.py
+++ test/1mrecord.py
@@ -2,12 +2,12 @@
 from zvtm.domain import Stock1mKdata,Block1mKdata,Index1mKdata,StockTradeDay,Stock1mHfqKdata,Stock5mHfqKdata,Stock15mHfqKdata,Stock30mHfqKdata,Stock1hHfqKdata,Stock4hHfqKdata,Block5mKdata,Block15mKdata,Block30mKdata,Block1hKdata
 import datetime
 # 东财无法获取历史板块数据 只能获取当天数据
-df0 = StockTradeDay.query_data(provider='joinquant',start_timestamp='2022-10-28',end_timestamp='2022-12-31')
+# df0 = StockTradeDay.query_data(provider='joinquant',start_timestamp='2022-10-28',end_timestamp='2022-12-31')
 # for i in range(1,len(df0),1):
 #     print(df0.iloc[-i]['timestamp'])
 #     if i < len(df0)-1:
 #         Block1mKdata.record_data(provider="em", code='BK0473',sleeping_time=1,start_timestamp = df0.iloc[-i]['timestamp'].strftime("%Y-%m-%d"))
-# Block1mKdata.record_data(provider='em', sleeping_time=1)
+Block1mKdata.record_data(provider='em',end_timestamp = '2026-06-20', sleeping_time=1,code='BK0473')
 # Block5mKdata.record_data(provider='em', sleeping_time=1,code = 'BK0473')
 # Block15mKdata.record_data(provider='em', sleeping_time=1,code = 'BK0473')
 # Block30mKdata.record_data(provider='em', sleeping_time=1,code = 'BK0473')
@@ -33,4 +33,4 @@ df0 = StockTradeDay.query_data(provider='joinquant',start_timestamp='2022-10-28'
 #
 # Stock1mHfqKdata.record_data(provider='joinquant',code='002761')
 
-Stock1mKdata.record_data(provider='em', codes='601788', sleeping_time=1)
+# Stock1mKdata.record_data(provider='em', codes='601788', sleeping_time=1)
